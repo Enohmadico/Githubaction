c958c21f456358478da84f9c45734707e00832db
Edwin enoh <careeredwin1996.@gmail.com>
2024-10-20 16:32:27 -0400

    readme file edited

--- script/script2.ps1
+++ script/script2.ps1
@@ -1,1 +1,9 @@
 echo "man"
+
+
+if ((git config --global user.email) -eq "edwinenoh1996@gmail.com") {
+    git config --global --unset user.email
+    Write-Host "Removed email: edwinenoh1996@gmail.com"
+} else {
+    Write-Host "Email not found or is different."
+}
